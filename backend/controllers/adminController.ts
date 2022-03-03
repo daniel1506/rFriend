733af0aa83e1a850a7529f7cf0dea785188cc366
mayingkei <mayingkei@gmail.com>
2022-03-03 17:03:58 +0800

    /fix #3
    fixed admin route user not found exception

--- backend/controllers/adminController.ts
+++ backend/controllers/adminController.ts
@@ -29,6 +29,16 @@ export const updateUser = async (req: Request, res: Response, next: NextFunction
 
   const { user_id, password } = req.body;
 
+  const user = await prisma.user.findUnique({
+    where: {
+      id: user_id,
+    },
+  })
+
+  if(!user){
+    return next(new HttpException(404, "User not found"));
+  }
+  
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
@@ -60,6 +70,16 @@ export const deleteUser = async (req: Request, res: Response, next: NextFunction
   }
 
   const user_id = req.body.user_id;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: user_id,
+    },
+  })
+
+  if(!user){
+    return next(new HttpException(404, "User not found"));
+  }
 
   let result;
   try {
