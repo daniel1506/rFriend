0b0adcbe7af5e6145886b69fb4cee31765aa8259
BeyondTheHorizon <70520198+daniel1506@users.noreply.github.com>
2022-03-12 22:38:39 +0800

    Event browser reupload

--- frontend/src/pages/Homepage/EventBrowser.jsx
+++ frontend/src/pages/Homepage/EventBrowser.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './EventBrowser.css';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 class EventBrowser extends React.Component {
     render() {
@@ -57,12 +72,88 @@ class EventFilter extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-6"></div>
+                <div className="col-md-6">
+                    <EventCard/>
+                </div>
             </div>
             </div>
             </div>
         );
     }
+}
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+export default function EventCard() {
+    const [expanded, setExpanded] = React.useState(false);
+  
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+  
+    return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="host_avatar">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Event Name"
+          subheader="Date"
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="src\images\cards\event.png"
+          alt="Event Name"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Event Location: 
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Event Description: 
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Event Tags</Typography>
+            <Typography paragraph>Event Comments</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    );
 }
 
 ReactDOM.render(
