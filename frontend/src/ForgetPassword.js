375a68647b6fd5a138f9eecfc2cdd71bc0022cc3
Man Ho <kodyglock@gmail.com>
2022-03-12 21:52:49 +0800

    navbar ui & reorganize files

--- frontend/src/ForgetPassword.js
+++ frontend/src/ForgetPassword.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { Button, Grid } from "@mui/material";
+//@ts-check
+import { React, useState } from "react";
+import {
+  Button,
+  Grid,
+  Slide,
+  Input,
+  TextField,
+  FormControl,
+} from "@mui/material";
 import KeyIcon from "@mui/icons-material/Key";
-function ForgetPassword() {
+function ForgetPassword(props) {
   return (
-    <Button>
-      <KeyIcon />
-      Forgot password?
-    </Button>
+    <>
+      <Button onClick={props.handleForget}>
+        <KeyIcon />
+        Forgot password?
+      </Button>
+      <Slide direction="up" in={props.forgetChecked} mountOnEnter unmountOnExit>
+        <form>
+          <TextField
+            error={props.emailError}
+            type="email"
+            helperText={props.emailError ? "Invalid email" : ""}
+            id="forget"
+            label="email"
+            className="info-input"
+            onChange={(e) => {
+              props.validateEmail(e);
+            }}
+          />
+          <Button type="submit">submit</Button>
+        </form>
+      </Slide>
+    </>
   );
 }
 
--- frontend/src/Logreg.jsx
+++ frontend/src/Logreg.jsx
@@ -1,5 +1,12 @@
 //@ts-check
-import { Button, ButtonGroup, Slide, TextField, Grid } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  Slide,
+  TextField,
+  Grid,
+  FormControl,
+} from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import ForgetPassword from "./ForgetPassword.js";
 import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
@@ -7,13 +14,15 @@ import * as React from "react";
 import "./App.css";
 import Axios from "axios";
 import validator from "validator";
+import { useEffect } from "react";
 function Logreg() {
   const [logChecked, setLogChecked] = React.useState(false);
   const [regChecked, setRegChecked] = React.useState(false);
+  const [forgetChecked, setForgetChecked] = React.useState(false);
   const [username, setUsername] = React.useState(null);
   const [email, setEmail] = React.useState(null);
   const [emailError, setEmailError] = React.useState(false);
-  const [logEmailError, setLogEmailError] = React.useState(false);
+  const [logEmail, setLogEmail] = React.useState(null);
   const [password, setPassword] = React.useState(null);
   const [passwordError, setPasswordError] = React.useState(false);
   const [cfPassword, setCfPassword] = React.useState(null);
@@ -21,25 +30,24 @@ function Logreg() {
   const handleLog = () => {
     setLogChecked((prev) => !prev);
     setRegChecked(false);
+    setForgetChecked(false);
   };
   const handleReg = () => {
     setRegChecked((prev) => !prev);
     setLogChecked(false);
+    setForgetChecked(false);
+  };
+  const handleForget = () => {
+    setForgetChecked((prev) => !prev); //used to toggle the value of forgetChecked
+    setLogChecked(false);
+    setRegChecked(false);
   };
   //provide validity checking for both email input at login & email input at register
   const validateEmail = (e) => {
-    var email = e.target.value;
-
-    if (validator.isEmail(email) && logChecked == true) {
-      setLogEmailError(false);
-    }
-    if (!validator.isEmail(email) && logChecked == true) {
-      setLogEmailError(true);
-    }
-    if (validator.isEmail(email) && regChecked == true) {
+    let email = e.target.value;
+    if (validator.isEmail(email)) {
       setEmailError(false);
-    }
-    if (!validator.isEmail(email) && regChecked == true) {
+    } else {
       setEmailError(true);
     }
   };
@@ -52,6 +60,9 @@ function Logreg() {
       setPasswordError(true);
     }
   };
+  useEffect(() => {
+    setEmailError(false);
+  }, [logChecked, regChecked, forgetChecked]); //Reset the error showing flag if the user give up to enter the current information
   const addUser = () => {
     console.log({ email, username, password });
     Axios.post("/api/user/register", { email, username, password })
@@ -78,7 +89,12 @@ function Logreg() {
           Register
         </Button>
       </ButtonGroup>
-      <ForgetPassword />
+      <ForgetPassword
+        handleForget={handleForget}
+        forgetChecked={forgetChecked}
+        emailError={emailError}
+        validateEmail={validateEmail}
+      />
       <Slide
         direction="up"
         in={logChecked}
@@ -86,34 +102,36 @@ function Logreg() {
         unmountOnExit
         className="info-input-container"
       >
-        <Grid
-          container
-          direction="column"
-          alignItems="center"
-          justify="center"
-          gap="10px"
-          marginTop="10px"
-        >
-          <TextField
-            error={logEmailError}
-            type="email"
-            helperText={logEmailError ? "Invalid email" : ""}
-            id="emailLog"
-            label="email"
-            className="info-input"
-            onChange={(e) => {
-              setPassword(e.target.value);
-              validateEmail(e);
-            }}
-          />
-          <TextField
-            id="passwordlog"
-            type="password"
-            label="password"
-            className="info-input"
-          />
-          <Button>Submit</Button>
-        </Grid>
+        <form>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justify="center"
+            gap="10px"
+            marginTop="10px"
+          >
+            <TextField
+              error={emailError}
+              type="email"
+              helperText={emailError ? "Invalid email" : ""}
+              id="emailLog"
+              label="email"
+              className="info-input"
+              onChange={(e) => {
+                setLogEmail(e.target.value);
+                validateEmail(e);
+              }}
+            />
+            <TextField
+              id="passwordlog"
+              type="password"
+              label="password"
+              className="info-input"
+            />
+            <Button type="submit">Submit</Button>
+          </Grid>
+        </form>
       </Slide>
       <Slide
         direction="up"
@@ -122,57 +140,61 @@ function Logreg() {
         unmountOnExit
         className="info-input-container"
       >
-        <Grid
-          container
-          direction="column"
-          alignItems="center"
-          justify="center"
-          gap="10px"
-          marginTop="10px"
-        >
-          <TextField
-            id="emailReg"
-            error={emailError}
-            helperText={emailError ? "Invalid email" : ""}
-            type="email"
-            label="email"
-            className="email-input"
-            onChange={(e) => {
-              setEmail(e.target.value);
-              validateEmail(e);
-            }}
-          />
-          <TextField
-            id="usernameReg"
-            label="username"
-            className="info-input"
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          />
-          <TextField
-            id="passwordReg"
-            type="password"
-            label="password"
-            className="info-input"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <TextField
-            id="cfPasswordReg"
-            error={passwordError}
-            helperText={passwordError ? "Doesn't match" : ""}
-            type="password"
-            label="confirm password"
-            className="info-input"
-            onChange={(e) => {
-              setCfPassword(e.target.value);
-              validatePassword(e);
-            }}
-          />
-          <Button onClick={addUser}>Submit</Button>
-        </Grid>
+        <form>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justify="center"
+            gap="10px"
+            marginTop="10px"
+          >
+            <TextField
+              id="emailReg"
+              error={emailError}
+              helperText={emailError ? "Invalid email" : ""}
+              type="email"
+              label="email"
+              className="email-input"
+              onChange={(e) => {
+                setEmail(e.target.value);
+                validateEmail(e);
+              }}
+            />
+            <TextField
+              id="usernameReg"
+              label="username"
+              className="info-input"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <TextField
+              id="passwordReg"
+              type="password"
+              label="password"
+              className="info-input"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <TextField
+              id="cfPasswordReg"
+              error={passwordError}
+              helperText={passwordError ? "Doesn't match" : ""}
+              type="password"
+              label="confirm password"
+              className="info-input"
+              onChange={(e) => {
+                setCfPassword(e.target.value);
+                validatePassword(e);
+              }}
+            />
+            <Button onClick={addUser} type="submit">
+              Submit
+            </Button>
+          </Grid>
+        </form>
       </Slide>
     </>
   );
