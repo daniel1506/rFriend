0b388666b6d7c721eb2665b4f723171d90af4a1d
mayingkei <mayingkei@gmail.com>
2022-03-28 15:44:14 +0800

    Fix admin delete user cascade delete #26

--- backend/controllers/adminController.ts
+++ backend/controllers/adminController.ts
@@ -91,16 +91,19 @@ export const deleteUser = async (req: Request, res: Response, next: NextFunction
     return next(new HttpException(404, "User not found"));
   }
 
-  let result;
-  try {
-    result = await prisma.user.delete({
-      where: {
-        id: user_id,
-      },
-    });
-  } catch (e) {
-    return next(prismaErrorHandler(e));
-  }
+  const deleteEvents = prisma.event.deleteMany({
+    where: {
+      ownerId: user_id,
+    },
+  });
+
+  const deleteUser = prisma.user.delete({
+    where: {
+      id: user_id,
+    },
+  });
+
+  const transaction = await prisma.$transaction([deleteEvents, deleteUser]);
 
   res.status(201).send({ message: "User deleted" });
 };
