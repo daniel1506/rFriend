de1c5d1e200e63dbcbe76588051f8a2841541650
mayingkei <mayingkei@gmail.com>
2022-04-10 03:23:11 +0800

    Fix eventComment relation

---
+++ backend/prisma/migrations/20220409191801_add_event_comment_relation/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `eventcomment` table. All the data in the column will be lost.
+  - Added the required column `ownerId` to the `EventComment` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `eventcomment` DROP COLUMN `userId`,
+    ADD COLUMN `ownerId` INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE `EventComment` ADD CONSTRAINT `EventComment_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
