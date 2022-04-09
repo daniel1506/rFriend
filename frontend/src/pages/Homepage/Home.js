57441f57ff68fdc55df4c12f3d4c67bb644f52c7
BeyondTheHorizon <70520198+daniel1506@users.noreply.github.com>
2022-04-09 23:13:15 +0800

    Add files via upload

--- frontend/src/pages/Homepage/Home.js
+++ frontend/src/pages/Homepage/Home.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { styled } from '@mui/material/styles';
-import EventCard from './EventCard.js';
+import EventCard from '../../components/EventCard.js';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Calendar from './Calendar.js';
-import Events from './assets/Events.js';
+import get from '../../get.js';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,6 +18,11 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function Home() {
+
+    const [eventList, setEventList] = React.useState([]);
+
+    useEffect(() => {get('https://rfriend.herokuapp.com/api/user/browse').then((r)=>{setEventList(r.event)});}, []);
+
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
@@ -27,10 +32,10 @@ export default function Home() {
                 </Grid>
                 <Grid item xs={4}>
                     <Stack spacing={2}>
-                        {Events.map((e, index)=>{
+                        {eventList.map((e, index)=>{
                             return (
-                                <EventCard key={index} eventName={e.eventName} hostName={e.hostName} eventTime={e.eventTime}
-                                eventLocation={e.eventLocation} eventCategory={e.eventCategory} maxParticipants={e.maxParticipants} eventRemark={e.eventRemark}/>
+                                <EventCard eventId={e.id} eventName={e.name} hostId={e.ownerId} eventTime={e.startsAt} isJoined={e.isEventJoined} isLiked={e.isEventLiked}
+                                host={e.owner} eventLocation={e.location} eventCategory={e.category} maxParticipants={e.maxParticipants} eventRemark={e.remarks}/>
                             );
                         })}
                     </Stack>
