6ec7af293822a9f0e50a59a323fd38cba8cdb98c
Thomas Leong <lch27thomas@gmail.com>
2022-04-10 12:45:03 +0800

    Fix EventCardSettingBtn use reserved keyword as function name

--- frontend/src/components/EventCardSettingBtn.js
+++ frontend/src/components/EventCardSettingBtn.js
@@ -1,76 +1,71 @@
-import React from 'react';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import IconButton from '@mui/material/IconButton';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import delete from '../lib/delete';
+import React from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import deleteReq from "../lib/delete";
 
 //import { withStyles } from 'material-ui/styles';
 
 class EventCardSettingBtn extends React.Component {
-    state = {
-        anchorEl: null
-    };
+  state = {
+    anchorEl: null,
+  };
 
-    handleChange = (event, checked) => {
-        this.setState({ auth: checked });
-    };
+  handleChange = (event, checked) => {
+    this.setState({ auth: checked });
+  };
 
-    handleMenu = event => {
-        this.setState({ anchorEl: event.currentTarget });
-    };
+  handleMenu = (event) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
-    handleClose = () => {
-        this.setState({ anchorEl: null });
-    };
-    
-    handleOnClick = (link) => {
-        if(link == 'Delete Event') {
-            delete('/api/event/'+this.props.eventId,{"event_id": this.props.eventId});
-        }
-        this.handleClose();
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleOnClick = (link) => {
+    if (link == "Delete Event") {
+      deleteReq("/api/event/" + this.props.eventId, { event_id: this.props.eventId });
     }
+    this.handleClose();
+  };
 
-    render() {
-        const { classes } = this.props;
-        const { auth, anchorEl } = this.state;
-        const open = Boolean(anchorEl);
-        const Wrapper = this.props.iconType;
-        const listItems = this.props.items.map((link) =>
-            <MenuItem onClick={this.handleOnClick(link)} key={link}>{link}</MenuItem>
-        );
+  render() {
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+    const Wrapper = this.props.iconType;
+    const listItems = this.props.items.map((link) => (
+      <MenuItem onClick={this.handleOnClick(link)} key={link}>
+        {link}
+      </MenuItem>
+    ));
 
-        return (
-            <div>
-                <IconButton
-                    aria-owns={open ? 'menu-appbar' : null}
-                    aria-haspopup="true"
-                    onClick={this.handleMenu}
-                >
-                    {<Wrapper />}
-                </IconButton>
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    open={open}
-                    onClose={this.handleClose}
-                >
-                    {listItems}
-
-
-                </Menu>
-            </div>
-        );
-    }
-
+    return (
+      <div>
+        <IconButton aria-owns={open ? "menu-appbar" : null} aria-haspopup="true" onClick={this.handleMenu}>
+          {<Wrapper />}
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={open}
+          onClose={this.handleClose}
+        >
+          {listItems}
+        </Menu>
+      </div>
+    );
+  }
 }
 
 export default EventCardSettingBtn;
