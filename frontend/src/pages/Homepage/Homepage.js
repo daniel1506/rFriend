126fae40bcb488efb5463c53a8119a40d8a6a1a4
Thomas Leong <lch27thomas@gmail.com>
2022-04-10 12:41:51 +0800

    Update homepage swap between map and calendar #56

--- frontend/src/pages/Homepage/Homepage.js
+++ frontend/src/pages/Homepage/Homepage.js
@@ -2,13 +2,12 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import { useContext, useState } from "react";
 import AuthContext from "../../store/auth-context";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import CreateEvent from "./CreateEvent";
 import Profile from "../../components/Profile";
 import MapsView from "./MapsView";
 import Home from "./Home";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import AccessAlarmOutlinedIcon from "@mui/icons-material/AccessAlarmOutlined";
+import GeneralContext from "../../store/general-context";
+
 function Homepage() {
   const [showCreateEvent, setShowCreateEvent] = useState(false);
   const [showProfile, setShowProfile] = useState(false);
@@ -18,24 +17,14 @@ function Homepage() {
     authCtx.logout();
   };
 
+  const { isMapView } = useContext(GeneralContext);
+
   return (
     <>
-      <Navbar
-        onLogout={onLogout}
-        setShowCreateEvent={setShowCreateEvent}
-        setShowProfile={setShowProfile}
-      />
-      <CreateEvent
-        showCreateEvent={showCreateEvent}
-        setShowCreateEvent={setShowCreateEvent}
-      />
-      <Profile
-        setShowProfile={setShowProfile}
-        showProfile={showProfile}
-        admin
-      />
-      <MapsView />
-      <Home />
+      <Navbar onLogout={onLogout} setShowCreateEvent={setShowCreateEvent} setShowProfile={setShowProfile} />
+      <CreateEvent showCreateEvent={showCreateEvent} setShowCreateEvent={setShowCreateEvent} />
+      <Profile setShowProfile={setShowProfile} showProfile={showProfile} admin />
+      {isMapView ? <MapsView /> : <Home />}
     </>
   );
 }
