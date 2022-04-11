48ba78c089bfebc01c347aa09ce75755a51e5c4d
BeyondTheHorizon <70520198+daniel1506@users.noreply.github.com>
2022-04-11 18:08:39 +0800

    Update Home.js

--- frontend/src/pages/Homepage/Home.js
+++ frontend/src/pages/Homepage/Home.js
@@ -59,6 +59,7 @@ export default function Home() {
                 eventCategory={e.category}
                 maxParticipants={e.maxParticipants}
                 eventRemark={e.remarks}
+                eventComment={e.comment}
               />
             );
           })}
