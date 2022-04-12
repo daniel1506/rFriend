063e62b413be4eb63ea783fe52f3fa3a853d0ed6
Man Ho <kodyglock@gmail.com>
2022-04-13 01:11:00 +0800

    fix liked event alignment

--- frontend/src/pages/Homepage/LikedEvents.js
+++ frontend/src/pages/Homepage/LikedEvents.js
@@ -34,23 +34,25 @@ const LikedEvents = () => {
           <>
             {likedOnes.map((event, index) => {
               return (
-                <Grid key={event.id} item>
-                  <EventCard
-                    eventId={event.id}
-                    eventName={event.name}
-                    hostId={event.ownerId}
-                    eventTime={event.startsAt}
-                    isJoined={event.isEventJoined}
-                    isLiked={event.isEventLiked}
-                    photoUrl={event.photoUrl}
-                    host={event.owner}
-                    eventLocation={event.location}
-                    eventCategory={event.category}
-                    participants={event.participants}
-                    maxParticipants={event.maxParticipants}
-                    eventRemark={event.remarks}
-                    eventComment={event.comment}
-                  />
+                <Grid key={event.id} item xs={12} sm={6} md={3}>
+                  <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    <EventCard
+                      eventId={event.id}
+                      eventName={event.name}
+                      hostId={event.ownerId}
+                      eventTime={event.startsAt}
+                      isJoined={event.isEventJoined}
+                      isLiked={event.isEventLiked}
+                      photoUrl={event.photoUrl}
+                      host={event.owner}
+                      eventLocation={event.location}
+                      eventCategory={event.category}
+                      participants={event.participants}
+                      maxParticipants={event.maxParticipants}
+                      eventRemark={event.remarks}
+                      eventComment={event.comment}
+                    />
+                  </Box>
                 </Grid>
               );
             })}
