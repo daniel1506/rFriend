2a5176c3872199c213cc4475e90fcad0bdd11f27
Man Ho <kodyglock@gmail.com>
2022-04-12 18:47:45 +0800

    fix cannot leave askVerify after enterring

--- frontend/src/pages/AskVerify.js
+++ frontend/src/pages/AskVerify.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import MarkEmailUnreadIcon from "@mui/icons-material/MarkEmailUnread";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Button } from "@mui/material";
 import VerticalFlex from "../layout/VerticalFlex";
+import AuthContext from "../store/auth-context";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import redirectToMailBox from "../lib/redirectToMailBox";
 function AskVerify() {
+  let authCtx = useContext(AuthContext);
   return (
-    <VerticalFlex height="100%">
+    <VerticalFlex height="100%" gap={2}>
       <MarkEmailUnreadIcon sx={{ fontSize: "200px" }} />
       <Typography variant="h3">You are not verified, please check your email</Typography>
+      <Button
+        variant="contained"
+        color="warning"
+        onClick={() => {
+          redirectToMailBox(authCtx.email);
+        }}
+      >
+        Check your email
+      </Button>
+      <Button
+        endIcon={<ExitToAppIcon />}
+        onClick={() => {
+          authCtx.logout();
+        }}
+      >
+        Login page
+      </Button>
     </VerticalFlex>
   );
 }
