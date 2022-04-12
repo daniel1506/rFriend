bc038947ee44c01b44fed030c7d52cd7fedcc3c4
Thomas Leong <lch27thomas@gmail.com>
2022-04-12 22:19:25 +0800

    Update Increase backend body size #68

--- backend/index.ts
+++ backend/index.ts
@@ -27,7 +27,7 @@ const app = express();
 
 app.use(helmet.contentSecurityPolicy(policy));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.use("/api", apiRouter);
 app.use("/", webRouter);
