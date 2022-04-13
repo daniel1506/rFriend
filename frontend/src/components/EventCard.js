d41e1539fc8cc7fe2f0cca58d6ecff6fdfce30c5
Man Ho <kodyglock@gmail.com>
2022-04-13 23:14:21 +0800

    fix join event dark mode color

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -32,6 +32,7 @@ import put from "../lib/put.js";
 //import MenuItem from '@mui/material/MenuItem';
 import EventCardSettingBtn from "./EventCardSettingBtn.js";
 import GeneralContext from "../store/general-context.js";
+import PrefContext from "../store/preference-context";
 import { DirectionsTransitFilledTwoTone } from "@mui/icons-material";
 import Profile from "./Profile.js";
 import { useContext } from "react";
@@ -79,6 +80,7 @@ const eventDetailItemStyle = (theme) => ({
 export default function EventCard(props) {
   //console.log(props.hostId);
   const generalCtx = useContext(GeneralContext);
+  const prefCtx = useContext(PrefContext);
   const isHost = localStorage.getItem("id") == props.hostId;
   const [expanded, setExpanded] = React.useState(false);
   const [showProfile, setShowProfile] = React.useState(false);
@@ -180,7 +182,7 @@ export default function EventCard(props) {
               ) : (
                 <svg width="24px" height="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                   <path
-                    style={{ fill: "rgba(0, 0, 0, 0.54)" }}
+                    style={{ fill: prefCtx.theme.palette.mode === "light" ? "#757575" : "#FFFFFF" }}
                     d="M224 232C237.3 232 248 242.7 248 256V304H296C309.3 304 320 314.7 320 328C320 341.3 309.3 352 296 352H248V400C248 413.3 237.3 424 224 424C210.7 424 200 413.3 200 400V352H152C138.7 352 128 341.3 128 328C128 314.7 138.7 304 152 304H200V256C200 242.7 210.7 232 224 232zM152 64H296V24C296 10.75 306.7 0 320 0C333.3 0 344 10.75 344 24V64H384C419.3 64 448 92.65 448 128V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V128C0 92.65 28.65 64 64 64H104V24C104 10.75 114.7 0 128 0C141.3 0 152 10.75 152 24V64zM48 448C48 456.8 55.16 464 64 464H384C392.8 464 400 456.8 400 448V192H48V448z"
                   />
                 </svg>
